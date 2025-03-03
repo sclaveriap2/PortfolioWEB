902a3933f5b7cf807d49fb788f709c1854992ca5
sebastian claveria <sebastian.claveria@inacapmail.cl>
2025-03-03 16:38:08 -0300

    Redirección a github y proof of concept juego

---
+++ Public/script.js
@@ -0,0 +1,15 @@
+const player = document.querySelector('.player');
+const container = document.querySelector('.game-container');
+const step = 20; // Tamaño del movimiento
+let x = 180;
+let y = 180;
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp' && y > 0) y -= step;
+    if (event.key === 'ArrowDown' && y < container.clientHeight - player.clientHeight) y += step;
+    if (event.key === 'ArrowLeft' && x > 0) x -= step;
+    if (event.key === 'ArrowRight' && x < container.clientWidth - player.clientWidth) x += step;
+
+    player.style.top = `${y}px`;
+    player.style.left = `${x}px`;
+});

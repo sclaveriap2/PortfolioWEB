67cffc279faa61d1d7f03641fef13df1a58ef7eb
sebastian claveria <sclaveriap@gmail.com>
2025-03-06 19:51:28 -0300

    direcciones juego 2

--- Public/game.js
+++ Public/game.js
@@ -32,96 +32,98 @@ let isMovingDown = false;
 let isMovingLeft = false;
 let isMovingRight = false;
 
+
+
 function preload() {
     // Aquí puedes cargar recursos si es necesario (imágenes, sonidos)
     this.load.image('playerSprite', 'Images/Logo-banner.png');
 }
 
 function create() {
-    // Crear un cuadrado rojo (jugador)
+
     player = this.add.sprite(400, 300, 'playerSprite');
     this.physics.add.existing(player);
     player.body.setCollideWorldBounds(true);
     player.setOrigin(0.5, 0.5); // Centrar el origen del sprite
     cursors = this.input.keyboard.createCursorKeys();
     // Asociar eventos a los botones HTML
-    document.getElementById('button1').addEventListener('mousedown', function() {
+    document.getElementById('buttonUp').addEventListener('mousedown', function() {
         console.log("Botón Arriba presionado");
         isMovingUp = true;
     });
 
-    document.getElementById('button1').addEventListener('mouseup', function() {
+    document.getElementById('buttonUp').addEventListener('mouseup', function() {
         console.log("Botón Arriba soltado");
         isMovingUp = false;
     });
 
-    document.getElementById('button2').addEventListener('mousedown', function() {
+    document.getElementById('buttonDown').addEventListener('mousedown', function() {
         console.log("Botón Abajo presionado");
         isMovingDown = true;
     });
 
-    document.getElementById('button2').addEventListener('mouseup', function() {
+    document.getElementById('buttonDown').addEventListener('mouseup', function() {
         console.log("Botón Abajo soltado");
         isMovingDown = false;
     });
 
-    document.getElementById('button3').addEventListener('mousedown', function() {
+    document.getElementById('buttonLeft').addEventListener('mousedown', function() {
         console.log("Botón Izquierda presionado");
         isMovingLeft = true;
     });
 
-    document.getElementById('button3').addEventListener('mouseup', function() {
+    document.getElementById('buttonLeft').addEventListener('mouseup', function() {
         console.log("Botón Izquierda soltado");
         isMovingLeft = false;
     });
 
-    document.getElementById('button4').addEventListener('mousedown', function() {
+    document.getElementById('buttonRight').addEventListener('mousedown', function() {
         console.log("Botón Derecha presionado");
         isMovingRight = true;
     });
 
-    document.getElementById('button4').addEventListener('mouseup', function() {
+    document.getElementById('buttonRight').addEventListener('mouseup', function() {
         console.log("Botón Derecha soltado");
         isMovingRight = false;
     });
 
 
-    document.getElementById('button1').addEventListener('touchstart', function() {
+    document.getElementById('buttonUp').addEventListener('touchstart', function() {
         console.log("Botón Arriba presionado");
         isMovingUp = true;
     });
 
-    document.getElementById('button1').addEventListener('touchend', function() {
+    document.getElementById('buttonUp').addEventListener('touchend', function() {
         console.log("Botón Arriba soltado");
         isMovingUp = false;
     });
 
-    document.getElementById('button2').addEventListener('touchstart', function() {
+    document.getElementById('buttonDown').addEventListener('touchstart', function() {
         console.log("Botón Abajo presionado");
         isMovingDown = true;
     });
 
-    document.getElementById('button2').addEventListener('touchend', function() {
+    document.getElementById('buttonDown').addEventListener('touchend', function() {
         console.log("Botón Abajo soltado");
         isMovingDown = false;
     });
 
-    document.getElementById('button3').addEventListener('touchstart', function() {
+    document.getElementById('buttonLeft').addEventListener('touchstart', function() {
         console.log("Botón Izquierda presionado");
         isMovingLeft = true;
     });
 
-    document.getElementById('button3').addEventListener('touchend', function() {
+    document.getElementById('buttonLeft').addEventListener('touchend', function() {
         console.log("Botón Izquierda soltado");
         isMovingLeft = false;
     });
 
-    document.getElementById('button4').addEventListener('touchstart', function() {
+    document.getElementById('buttonRight').addEventListener('touchstart', function() {
         console.log("Botón Derecha presionado");
         isMovingRight = true;
     });
 
-    document.getElementById('button4').addEventListener('touchend', function() {
+    document.getElementById('buttonRight').addEventListener('touchend', function() {
         console.log("Botón Derecha soltado");
         isMovingRight = false;
     });
